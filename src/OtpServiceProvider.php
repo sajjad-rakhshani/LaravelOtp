09177d64c3f6629aa198ad172393b5b0fdba5734
sajjad-rakhshani <sajjad.rakhshani98@gmail.com>
2024-05-20 17:06:18 +0330

    refactor

--- src/OtpServiceProvider.php
+++ src/OtpServiceProvider.php
@@ -23,7 +23,7 @@ class OtpServiceProvider extends ServiceProvider
     public function boot() : void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        $this->publishesMigrations([
+        $this->publishes([
             __DIR__.'/../database/migrations' => database_path('migrations')
         ], 'laravel-otp-migrations');
         $this->publishes([
